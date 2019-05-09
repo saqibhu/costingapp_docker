cb8d77e99da8da12be72f06f395dd1f5e7a57fb3
Saqib Hussain <saqib.hussain_@hotmail.co.uk>
2019-05-09 08:56:51 +0100

    added a dcoker environment for production

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -5,15 +5,10 @@ then
     echo "Waiting for postgres..."
 
     #while ! nc -z $SQL_HOST $SQL_PORT; do
-    #  sleep 0.1
+     # sleep 0.1
     #done
 
     echo "PostgreSQL started"
 fi
-
-#clear the database each time the container is run
-#python manage.py flush --no-input
-
-python manage.py migrate
 
 exec "$@"
